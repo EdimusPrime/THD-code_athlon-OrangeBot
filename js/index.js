42668b568ab24e1c908f3a58567a9753c96829d6
EdimusPrime <edimusprime@Edimus-Prime.local>
2017-09-17 09:07:17 -0400

    THD-Code_athlon
    Code_athlon event. Team created Twitterbot. This file is for a mock
    monitoring of the twitter bot and an activity log

---
+++ js/index.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+
+//TWITTER WIDET TO PULL TWEETS 
+
+twttr.widgets.createTimeline(
+  {
+    sourceType: "profile",
+    screenName: "TwitterDev"
+  },
+  document.getElementById("container")
+);
+
+
+// PULLS RANDOM TICKET NUMBERS,NAMES, UPDATED DATES & STATUS 
+// TO POPULATE FICTIONAL ACTIVITY LOG
+
+$.ajax({
+  url: 'https://randomuser.me/api/?results=25',
+  dataType: 'json',
+  success: function(data) {
+    console.log(data);
+
+    for (var i=0; i<data.results.length; i++){
+
+    	var status = Math.floor(Math.random() * 400);
+    	var ticket = data.results[i].id.value;
+    	var lastName = data.results[i].name.last;
+    	var updateDate = data.results[i].registered;
+    	
+
+		if (status<201){ 
+			status="closed"
+			// $(".status").css("color", "red")
+
+		} else {
+			status="open"
+			// $(".status").css("color", "green")	
+		}
+
+		if (ticket == null){
+			var ticket = Math.floor(Math.random() * 900000000000);
+		}
+
+
+  		$(".activity_log").prepend(
+  			"<div class = 'row'>" + "<a href=#><div class='ticket col-md-3 '>" + ticket + "</div></a>"+
+  			"<div class='last_name col-md-3 '>" + lastName + "</div>" +
+  			"<div class='date_updated col-md-3 '>" + updateDate + "</div>" +
+  			"<div class='status col-md-3 '>" + status + "</div>" + "</div>"
+  			);
+
+
+
+  // 		function statusCheck(){
+  // 			event.preventDefault();
+  // 		if (status==="closed"){
+		// 	$(".ticket").css("text-decoration", "line-through");
+		// } else {
+		// 	$(".ticket").css("text-decoration", "none");
+		// }
+  //   }
+    }	
+statusCheck;
+
+  }
+});
+
+console.log(data);
+
+});
